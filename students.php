3c5b5ebc2551a7cf0d5c370bd45954b4a9e2305d
tobicg31 <82407605+tobicg31@users.noreply.github.com>
2025-06-07 23:45:41 -0300

    Add files via upload

---
+++ students.php
@@ -0,0 +1,79 @@
+<?php
+/**
+*    File        : backend/models/students.php
+*    Project     : CRUD PHP
+*    Author      : Tecnologías Informáticas B - Facultad de Ingeniería - UNMdP
+*    License     : http://www.gnu.org/licenses/gpl.txt  GNU GPL 3.0
+*    Date        : Mayo 2025
+*    Status      : Prototype
+*    Iteration   : 3.0 ( prototype )
+*/
+
+function getAllStudents($conn) 
+{
+    $sql = "SELECT * FROM students";
+
+    //MYSQLI_ASSOC devuelve un array ya listo para convertir en JSON:
+    return $conn->query($sql)->fetch_all(MYSQLI_ASSOC);
+}
+
+function getStudentById($conn, $id) 
+{
+    $stmt = $conn->prepare("SELECT * FROM students WHERE id = ?");
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    //fetch_assoc() devuelve un array asociativo ya listo para convertir en JSON de una fila:
+    return $result->fetch_assoc(); 
+}
+
+function createStudent($conn, $fullname, $email, $age) 
+{
+    $sql = "INSERT INTO students (fullname, email, age) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssi", $fullname, $email, $age);
+    $stmt->execute();
+
+    //Se retorna un arreglo con la cantidad e filas insertadas 
+    //y id insertado para validar en el controlador:
+    return 
+    [
+        'inserted' => $stmt->affected_rows,        
+        'id' => $conn->insert_id
+    ];
+}
+
+function updateStudent($conn, $id, $fullname, $email, $age) 
+{
+    $sql = "UPDATE students SET fullname = ?, email = ?, age = ? WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssii", $fullname, $email, $age, $id);
+    $stmt->execute();
+
+    //Se retorna fila afectadas para validar en controlador:
+    return ['updated' => $stmt->affected_rows];
+}
+
+function deleteStudent($conn, $id) 
+{
+    // Verificar si existe una relación con esa materia
+    $check = $conn->prepare("SELECT id FROM students_subjects WHERE student_id = ?");
+    $check->bind_param("i", $id);
+    $check->execute();
+    $res = $check->get_result();
+
+    if ($res->num_rows > 0) {
+        return ['deleted' => 0, 'error' => 'No se puede borrar la materia: está asignada a estudiantes.'];
+    }
+
+
+    $sql = "DELETE FROM students WHERE id = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+
+    //Se retorna fila afectadas para validar en controlador
+    return ['deleted' => $stmt->affected_rows];
+}
+?>
